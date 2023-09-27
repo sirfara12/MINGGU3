63e73c499cd6848f915eab912a6ee09590ef7971
sirfara <you@msirvaratigmail.com>
2023-09-27 11:45:37 +0700

    jobsheet 5

--- mingguke5/pemilihanpercobaan228.java
+++ mingguke5/pemilihanpercobaan228.java
@@ -20,19 +20,19 @@ public class pemilihanpercobaan228 {
         System.out.println("nilai akhir = " + total + " sehingga " + message);
         
         if (total >= 100) {
-            System.out.println("kualifikasi nilai sangat baik dengan predikat 'A' ");
+            System.out.println("kualifikasi nilai sangat baik dengan predikat 'A', nilai setara '4' ");
         } else if (total >=73) {
-            System.out.println("kualifikasi nilai lebih dari baik dengan predikat 'B+'  ");
+            System.out.println("kualifikasi nilai lebih dari baik dengan predikat 'B+', nilai setara '3.5' ");
         } else if (total >=63) {
-            System.out.println("kualifikasi nilai baik dengan predikat 'B' ");
+            System.out.println("kualifikasi nilai baik dengan predikat 'B', nilai setara '3' ");
         } else if (total >=60) {
-            System.out.println("kualifikasi nilai lebih dari cukup dengan predikat 'C+' ");
+            System.out.println("kualifikasi nilai lebih dari cukup dengan predikat 'C+', nilai setara '2.5' ");
         } else if (total >=50) {
-            System.out.println("kualifikasi nilai cukup dengan predikat 'C' ");
+            System.out.println("kualifikasi nilai cukup dengan predikat 'C', nilai setara '2' ");
         } else if (total >=39) {
-            System.out.println("kualifikasi nilai kurang dengan predikat 'D' ");
+            System.out.println("kualifikasi nilai kurang dengan predikat 'D', nilai setara '1' ");
         } else if (total <=39) {
-            System.out.println("kualifikasi nilai lebih dari kurang dengan predikat 'E' ");
+            System.out.println("kualifikasi nilai lebih dari kurang dengan predikat 'E', nilai setara '0' ");
         }
     }
 }
